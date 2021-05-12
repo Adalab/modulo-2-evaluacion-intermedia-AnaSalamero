e024ae8fdf8d5aab50454b19a2ab31e9c46b714e
Ana Salamero <asalamerosanz@gmail.com>
2021-05-12 10:10:37 +0200

    added counterTries function

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const input = document.querySelector('.js-input');
 const button = document.querySelector('.js-btn');
 const clue = document.querySelector('.js-clue');
 const counter = document.querySelector('.js-counter');
+let tries = 0;
 
 function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
@@ -15,7 +16,6 @@ console.log(`Mi numero aleatorio es ${randomNumber}`);
 
 function chooseANumber () {
     const selectedNumber = parseInt(input.value);
-    console.log(parseInt(input.value));
     if (selectedNumber < 1 || selectedNumber > 100) {
         clue.innerHTML = 'El numero debe estar entre 1 y 100';
     } else if (selectedNumber === randomNumber) {
@@ -28,7 +28,7 @@ function chooseANumber () {
 }
 
 const counterTries = () => {
-    let tries = 0;
+    tries++;
     counter.innerHTML = `Numero de intentos: ${tries}`;
 }
 
